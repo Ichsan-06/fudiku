187e9f13930b81646b2f4cc3732d6edeb8b61859
Ichsan-06 <013ichsanm@gmail.com>
2021-03-29 15:34:52 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ Route::get('/email',function(){
                 'subsName'=>"HAh",
             );
         // return new \App\Mail\WelcomeMail($email_data);
-       $data =  Mail::to("013ichsanm@gmail.com")->send(new \App\Mail\WelcomeMail($email_data));
+       $data =  Mail::to("018ichsanm@gmail.com")->send(new \App\Mail\WelcomeMail($email_data));
             if ($data) {
                 echo "Ber";
         }
