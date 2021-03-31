1355a170b0997db9171579a6edcb6dc7ddbce010
Ichsan-06 <013ichsanm@gmail.com>
2021-03-31 16:05:35 +0700

    Update RegisterController.php
    Sudah Bisa Daftar Spasi

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -50,7 +50,7 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'username' => ['alpha_dash','required', 'max:25', 'unique:users,username'],
+            'username' => ['required', 'max:25', 'unique:users,username'],
             'email' => ['required', 'email', 'max:255', 'unique:users,email'],
             'password' => ['required','min:6'],
             're-password'=>['required','min:6','same:password']
